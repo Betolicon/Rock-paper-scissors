aaaab59692e2f47409e25d4470c2d10068719c0b
Betolicon <[albertlicontti@gmail.com]>
2024-08-02 20:46:03 -0400

    I created a function for the humanSelection and its validation

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+const choices = ['rock', 'paper', 'scissors'];
+
 const getComputerChoice = () => {
-    const choices = ['rock', 'paper', 'scissors'];
     let choice = Math.floor(Math.random()*3);
-    console.log(choice)
     return choices[choice];
 }
 
+const ValidationHumanChoice = humanChoice =>{
+    for(let i = 0; i < choices.length; i++){
+        if(humanChoice === choices[i])
+            return true;
+    }
+    return false
+}
+
+const getHumanChoice = () =>{
+    let humanChoice = prompt("Enter your choice (rock, paper, or scissors):").toLowerCase()
+    while (ValidationHumanChoice(humanChoice)  === false)
+        humanChoice = prompt("Try again. Enter your choice (rock, paper, or scissors):").toLowerCase()
+    return humanChoice;
+}
+
+const humanSelection = getHumanChoice()
 const computerSelection = getComputerChoice()
-console.log(computerSelection)
