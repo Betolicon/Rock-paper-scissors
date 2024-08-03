0a3c39b5f6ccf9504f12dfa92b36a8bbd343cb0a
Betolicon <[albertlicontti@gmail.com]>
2024-08-02 21:01:34 -0400

    I just added the logic of the game

--- script.js
+++ script.js
@@ -20,5 +20,36 @@ const getHumanChoice = () =>{
     return humanChoice;
 }
 
+const playRound = (humanSelection, computerSelection) =>{
+    let humanScore = 0
+    let computerScore = 0
+    if(humanSelection == 'rock' && computerSelection == 'scissors'){
+        console.log("You win!! Rock beats scissors")
+        humanScore ++}
+    else if(humanSelection == 'scissors' && computerSelection == 'rock'){
+        console.log("You lose!! Rock beats scissors")
+        computerScore ++}
+    else if(humanSelection == 'paper' && computerSelection == 'rock'){
+        console.log("You win!! Paper beats rock")
+        humanScore ++}       
+    else if(humanSelection == 'rock' && computerSelection == 'paper'){
+        console.log("You lose!! Paper beats rock")
+        computerScore ++}
+    else if(humanSelection == 'scissors' && computerSelection == 'paper'){
+        console.log("You win!! Scissors beats paper")
+        humanScore ++}       
+    else if(humanSelection == 'paper' && computerSelection == 'scissors'){
+        console.log("You lose!! Scissors beats paper")
+        computerScore ++}
+    else{
+        console.log("It's a tie")
+        humanScore ++
+        computerScore ++
+    }
+    console.log(`Your score: ${humanScore}`)
+    console.log(`Computer score: ${computerScore}`)
+}
+
 const humanSelection = getHumanChoice()
 const computerSelection = getComputerChoice()
+playRound(humanSelection, computerSelection)
